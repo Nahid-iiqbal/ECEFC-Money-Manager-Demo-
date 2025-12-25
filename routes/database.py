e780d223e850a7584bb4f2e8bd20bd051c5cf3df
Tahsinul2K <tahsinuljubair@gmail.com>
2025-12-25 13:45:32 +0600

    Expenses
    Integrated Database in the expense.py and added expense class in database.py

--- routes/database.py
+++ routes/database.py
@@ -10,6 +10,13 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(15), unique=True, nullable=False)
     password_hash = db.Column(db.String(80), nullable=False)
 
+class Expense(db.Model):
+    """Expense model"""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 def init_db(app):
     """Initialize the database"""
--- routes/expense.py
+++ routes/expense.py
@@ -1,29 +1,49 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_required, current_user
+from routes.database import db, Expense
 
 expense = Blueprint("expense", __name__)
 
-expenses = []  # In-memory list to store expenses for testing purposes
+expenses = {}  # In-memory list to store expenses for testing purposes
 @expense.route('/expenses')
+@login_required
 def view_expenses():
-    total = 0
-    for val in expenses:
-        total += val['amount']
-    return render_template("expenses.html", expenses=expenses, total=total)
+    user_expenses = Expense.query.filter_by(
+        user_id=current_user.id
+    ).all()
+    total =0
+    for val in user_expenses:
+        total += val.amount
+    return render_template("expenses.html", expenses=user_expenses, total=total)
 
 @expense.route('/add_expense', methods=['GET', 'POST'])
+@login_required
 def add_expense():
     name = request.form['name']
     amount = float(request.form['amount'])
-    expenses.append({'name': name, 'amount': amount})
+    new_expense = Expense(name=name, amount=amount, user_id=current_user.id)
+    db.session.add(new_expense)
+    db.session.commit()
+
     return redirect(url_for('expense.view_expenses'))
 
 @expense.route("/clear_expenses", methods=['POST'])
+@login_required
 def clear_expenses():
-    expenses.clear()
+    Expense.query.filter_by(user_id=current_user.id).delete()
+    db.session.commit()
+
     return redirect(url_for('expense.view_expenses'))
 
 @expense.route('/expenses_pop', methods=['POST'])
+
+@login_required
 def clear_pop_expense():
-    idx = int(request.form['idx'])
-    expenses.pop(idx)
+    expense_id = int(request.form['id'])
+    expense = Expense.query.filter_by(id = expense_id, user_id = current_user.id).first()
+
+    if expense:
+        db.session.delete(expense)
+        db.session.commit()
+    
     return redirect(url_for('expense.view_expenses'))
